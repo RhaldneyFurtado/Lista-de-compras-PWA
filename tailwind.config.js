ac593252e386bc36d472bebb163cb857ae45f8f2
RhaldneyFurtado <rhaldney.furtado@gmail.com>
2026-05-17 01:20:42 -0300

    Ativando o modo dark no Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // ativa dark mode por classe
+  darkMode: "class",
+
   // Arquivos que o Tailwind vai escanear
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
-  // Tema do Tailwind
   theme: {
     extend: {
-      // Aqui você pode customizar cores, fontes, etc
       colors: {
         primaria: "#10b981", // verde principal do app
       },
     },
   },
 
-  // Plugins do Tailwind (se precisar futuramente)
   plugins: [],
 };
